0b3dd19edd63dc65fea0f9b00e99ceac08575aca
egor <sharapov.egor00@gmail.com>
2023-11-07 00:01:52 +0300

    Change functions

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,22 +1,19 @@
 import pytest
+from pathlib import Path
+import json
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator, transactions_list
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
 @pytest.fixture
 def get_data() -> list[dict]:
+    transactions_list = json.loads(Path("../data", "transactions.json").read_text())
     return transactions_list
 
 
 @pytest.fixture
 def get_expected_result_filter() -> list[int]:
     return [939719570, 142264268, 895315941]
-
-
-def test_filter_by_currency(get_data: list[dict], get_expected_result_filter: list[int]) -> None:
-    usd_transactions = filter_by_currency(transactions_list, "USD")
-    for i in range(3):
-        assert next(usd_transactions)["id"] == get_expected_result_filter[i]
 
 
 @pytest.fixture
@@ -28,15 +25,27 @@ def get_expected_result_descriptions() -> list[str]:
             "Перевод организации"]
 
 
+@pytest.fixture
+def get_get_expected_result_card() -> list[str]:
+    def get_bank_card(card: int) -> str:
+        bank_card = str(card)
+        while len(bank_card) != 16:
+            bank_card = "0" + bank_card
+        return f"{bank_card[:4]} {bank_card[4:8]} {bank_card[8:12]} {bank_card[12:]}"
+
+    return list(map(get_bank_card, range(1, 6)))
+
+
+def test_filter_by_currency(get_data: list[dict], get_expected_result_filter: list[int]) -> None:
+    usd_transactions = filter_by_currency(get_data, "USD")
+    for i in range(3):
+        assert next(usd_transactions)["id"] == get_expected_result_filter[i]
+
+
 def test_transaction_descriptions(get_data: list[dict], get_expected_result_descriptions: list[str]) -> None:
     descriptions = transaction_descriptions(get_data)
     for i in range(5):
         assert next(descriptions) == get_expected_result_descriptions[i]
-
-
-@pytest.fixture
-def get_get_expected_result_card() -> list[str]:
-    return [f"0000 0000 0000 000{i}" for i in range(1, 6)]
 
 
 def test_card_number_generator(get_get_expected_result_card: list[str]) -> None:

144bfb43fd283cdd7ec0642cf2823e01671069a2
egor <sharapov.egor00@gmail.com>
2023-11-14 21:31:45 +0300

    Change decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -14,21 +14,22 @@ def log(filename: None | str) -> Callable:
     def wrapper(func: Callable) -> Callable:
         @wraps(func)
         def inner(*args: Any, **kwargs: Any) -> None:
-            if not filename:
-                try:
-                    func(*args, **kwargs)
+            try:
+                func(*args, **kwargs)
+                if not filename:
                     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {func.__name__} ok")
-                except Exception as error:
+                else:
+                    data_result = Path(Path.home(), "PycharmProjects", "fintech_app", "data", filename)
+                    data_result.write_text(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {func.__name__} ok")
+
+            except Exception as error:
+                if not filename:
                     print(
                         f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {func.__name__} "
                         f"error: {error} Inputs: {args} {kwargs}")
+                else:
+                    data_result = Path(Path.home(), "PycharmProjects", "fintech_app", "data", filename)
 
-            else:
-                data_result = Path(Path.home(), "PycharmProjects", "fintech_app", "data", filename)
-                try:
-                    func(*args, **kwargs)
-                    data_result.write_text(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {func.__name__} ok")
-                except Exception as error:
                     data_result.write_text(
                         f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {func.__name__} "
                         f"error: {error} Inputs: {args} {kwargs}")
@@ -38,7 +39,7 @@ def log(filename: None | str) -> Callable:
     return wrapper
 
 
-@log(filename="log.txt")
+@log(filename=None)
 def my_function(x: int | float, y: int | float) -> int | float:
     """Возвращает сумму аргументов"""
     return x + y

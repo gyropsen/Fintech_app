c457378c9540b1e6bad5fb995068f95afe016f1e
egor <sharapov.egor00@gmail.com>
2023-11-21 13:34:57 +0300

    Add utils.py and test_utils.py

---
+++ src/utils.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import json
+
+
+def get_transactions_list(path: str) -> list:
+    """
+    Принимает на вход путь до JSON-файла и возвращает список словарей с данными о финансовых транзакциях
+    :param path: Путь до JSON-файла
+    :return: Список словарей транзакций
+    """
+    try:
+        transactions_list = json.loads(
+            Path(path).read_text())
+        return transactions_list
+    except Exception as error:
+        print(error)
+        return []
+
+
+def get_amount_transaction(transaction: dict) -> float | ValueError:
+    """
+    Принимает на вход одну транзакцию и возвращает сумму транзакции в рублях или ошибку ValueError
+    :param transaction: Словарь транзакции
+    :return: Сумма транзакции
+    """
+    if not transaction:
+        raise ValueError("Пустой список")
+    elif not transaction["operationAmount"]["currency"]["code"] == "RUB":
+        return transaction["operationAmount"]["amount"]
+
+    raise ValueError("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
---
+++ tests/test_utils.py
@@ -0,0 +1,33 @@
+import pytest
+from pathlib import Path
+import json
+from src.utils import get_amount_transaction, get_transactions_list
+
+
+@pytest.fixture
+def get_data():
+    return json.loads(
+        Path(Path.home(), "PycharmProjects", "fintech_app", "data", "operations.json").read_text())
+
+
+def test_get_transactions_list(get_data):
+    assert get_data == get_transactions_list("/home/egor/PycharmProjects/fintech_app/data/operations.json")
+    assert [] == get_transactions_list("")
+    assert [] == get_transactions_list("/home/egor/PycharmProjects/fintech_app/data/None.json")
+    assert [] == get_transactions_list("/home/egor/PycharmProjects/fintech_app/data/not_list.json")
+
+
+def test_get_amount_transaction(get_data):
+    assert get_data[0]["operationAmount"]["amount"] == get_amount_transaction(get_data[0])
+
+
+def test_get_amount_transaction_value_error(get_data):
+    with pytest.raises(ValueError) as exinfo:
+        get_amount_transaction(get_data[1])
+
+    assert "Транзакция выполнена не в рублях. Укажите транзакцию в рублях" in str(exinfo.value)
+
+    with pytest.raises(ValueError) as exinfo:
+        get_amount_transaction(get_transactions_list("/home/egor/PycharmProjects/fintech_app/data/None.json"))
+
+    assert "Пустой список" in str(exinfo.value)

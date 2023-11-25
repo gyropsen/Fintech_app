3a1c41ce64db545aaf8ae837154f9e2da6a9b460
egor <sharapov.egor00@gmail.com>
2023-11-25 04:09:05 +0300

    Add logger.py masks.py and utils.py. Logging modules

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import json
 from typing import Any
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def get_transactions_list(path: str) -> Any:
@@ -9,12 +12,14 @@ def get_transactions_list(path: str) -> Any:
     :param path: Путь до JSON-файла
     :return: Список словарей транзакций
     """
+    logger.info("Start get_transactions_list")
     try:
         transactions_list = json.loads(
             Path(path).read_text())
+        logger.info("get_transactions_list successfully")
         return transactions_list
     except Exception as error:
-        print(error)
+        logger.error(f"get_transactions_list error: {error}")
         return []
 
 
@@ -24,9 +29,14 @@ def get_amount_transaction(transaction: dict) -> Any:
     :param transaction: Словарь транзакции
     :return: Сумма транзакции
     """
+    logger.info("Start get_amount_transaction")
     if not transaction:
+        logger.error("get_amount_transaction error: ValueError")
         raise ValueError("Пустой список")
+
     elif transaction["operationAmount"]["currency"]["code"] == "RUB":
+        logger.info("get_amount_transaction successfully")
         return transaction["operationAmount"]["amount"]
 
+    logger.error("get_amount_transaction error: ValueError")
     raise ValueError("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
--- tests/test_masks.py
+++ tests/test_masks.py
@@ -9,9 +9,9 @@ def data_test() -> list[str]:
 
 def test_get_mask_bank_card(data_test: list[str]) -> None:
     assert get_mask_bank_card(data_test[0]) == "1234 56** **** 3456"
-    assert get_mask_bank_card(data_test[1]) == "Введен некорректный номер банковской карты"
+    assert get_mask_bank_card(data_test[1]) == ""
 
 
 def test_get_mask_bank_account(data_test: list[str]) -> None:
-    assert get_mask_bank_account(data_test[0]) == "Введен некорректный номер счёта"
+    assert get_mask_bank_account(data_test[0]) == ""
     assert get_mask_bank_account(data_test[1]) == "**7890"

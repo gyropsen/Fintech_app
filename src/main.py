90b19301bb7299f3e22eeed4a14b4ea4a03637d4
egor <sharapov.egor00@gmail.com>
2023-11-28 00:19:10 +0300

    change main.py

--- src/main.py
+++ src/main.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     get_mask_bank_card("1234567891234567")
     get_mask_bank_account("")
     get_transactions_list("")
-    get_amount_transaction("")
+    get_amount_transaction(get_transactions_list(""))

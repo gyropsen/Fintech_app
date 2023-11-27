677cdd3c0cb6a0ce81c748e340ec2a47c1d66336
egor <sharapov.egor00@gmail.com>
2023-11-27 17:23:40 +0300

    Add files

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,6 @@
 from pathlib import Path
 import json
 from typing import Any
-import logging
-import pandas as pd
-
-logger = logging.getLogger(__name__)
 
 
 def get_transactions_list(path: str) -> Any:
@@ -13,14 +9,12 @@ def get_transactions_list(path: str) -> Any:
     :param path: Путь до JSON-файла
     :return: Список словарей транзакций
     """
-    logger.info("Start get_transactions_list")
     try:
         transactions_list = json.loads(
             Path(path).read_text())
-        logger.info("get_transactions_list successfully")
         return transactions_list
     except Exception as error:
-        logger.error(f"get_transactions_list error: {error}")
+        print(error)
         return []
 
 
@@ -30,48 +24,9 @@ def get_amount_transaction(transaction: dict) -> Any:
     :param transaction: Словарь транзакции
     :return: Сумма транзакции
     """
-    logger.info("Start get_amount_transaction")
     if not transaction:
-        logger.error("get_amount_transaction error: ValueError")
         raise ValueError("Пустой список")
-
     elif transaction["operationAmount"]["currency"]["code"] == "RUB":
-        logger.info("get_amount_transaction successfully")
         return transaction["operationAmount"]["amount"]
 
-    logger.error("get_amount_transaction error: ValueError")
     raise ValueError("Транзакция выполнена не в рублях. Укажите транзакцию в рублях")
-
-
-def get_list_transactions(not_formatted_list: list[dict]) -> list[dict]:
-    list_transactions = []
-    for transaction in not_formatted_list:
-        list_transactions.append({'id': transaction['id'],
-                                  'state': transaction['state'],
-                                  'date': transaction['date'],
-                                  'operationAmount': {'amount': transaction['amount'],
-                                                      'currency': {'name': transaction['currency_name'],
-                                                                   'code': transaction['currency_code']}},
-                                  'description': transaction['description'],
-                                  'from': transaction['from'],
-                                  'to': transaction['to']})
-    logger.info("read_csv_or_xlsx successfully")
-    return list_transactions
-
-
-def read_csv_or_xlsx(path: str) -> Any:
-    """
-    Чтение финансовых операций с CSV- и XLSX-файлов
-    :param path: Путь до файла
-    :return: dataframe
-    """
-    logger.info("Start read_csv_or_xlsx")
-
-    try:
-        if ".csv" in path:
-            return get_list_transactions(pd.read_csv(path, delimiter=";").to_dict("records"))
-        else:
-            return get_list_transactions(pd.read_excel(path).to_dict("records"))
-    except Exception as error:
-        logger.error(f"read_csv_or_xlsx error: {error}")
-        return error

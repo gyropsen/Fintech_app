4e1caced03d5276640c8098e5c12156ab53a7b04
egor <sharapov.egor00@gmail.com>
2023-11-29 00:31:20 +0300

    add search_description, get_statistics_category

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,8 @@
+import re
+from collections import Counter
+from typing import Any
+
+
 def get_dicts_by_key(dicts: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Возвращает список словарей, полученных по ключу состояния
@@ -17,3 +22,13 @@ def get_dicts_sort_by_date(dicts: list[dict], reverse: bool = True) -> list[dict
     :return: Список отсортированных словарей
     """
     return sorted(dicts, key=lambda x: x["date"], reverse=reverse)
+
+
+def search_description(transactions_list: list[dict], pattern: Any) -> list[dict]:
+    pattern = re.compile(pattern)
+    return [transaction for transaction in transactions_list if pattern.search(transaction['description'])]
+
+
+def get_statistics_category(transactions_list: list[dict]) -> dict:
+    counted = Counter(transaction['description'] for transaction in transactions_list)
+    return dict(counted)
